7c2bf60ea81f3f7e8d40e237f32f2659dda82880
Modesto Castrillón (División de Robótica y Oceanografía Computacional - SIANI - ULPGC) <otsedom@gmail.com>
2021-03-24 16:38:14 +0000

    Revisiones y adiciones

--- P9/p9_shader_setvalues_coord/ColoreaInput.glsl
+++ P9/p9_shader_setvalues_coord/ColoreaInput.glsl
@@ -7,5 +7,6 @@ uniform float u_time;
 
 void main() {
 	vec2 st = gl_FragCoord.xy/u_resolution;
-	gl_FragColor = vec4(st.x,st.y,abs(sin(u_time)),1.0);
+	//gl_FragColor = vec4(st.x,st.y,abs(sin(u_time)),1.0);
+gl_FragColor = vec4(st.x,st.y,0.0,1.0);
 }
